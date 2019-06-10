2dceed461a6d12ab0139ae4dee82dd73648858ce
Stephan Lunghi <slunghi43@gmail.com>
2019-06-10 13:13:29 -0400

    Added separation with writelines.

--- LinqExercises/Program.cs
+++ LinqExercises/Program.cs
@@ -28,32 +28,32 @@ namespace LinqExercises
 
             var L1 = Enumerable.Range(0, 10);
             var L2 = Enumerable.Range(10, 10);
-            
-            // Number 1
-            var number1 = L1.Sum();
 
+            // Number 1
+            Console.WriteLine("Number 1");
+            var number1 = L1.Sum();
             Console.WriteLine(number1);
             Console.WriteLine("_______________________________________");
             // Number 2
+            Console.WriteLine("Number 2");
             var number2 = L1.Aggregate((x, y) => (x + y));
 
             Console.WriteLine(number2);
             Console.WriteLine("_______________________________________");
             // Number 3
+            Console.WriteLine("Number 3");
             L2.ToList().ForEach(Console.WriteLine);
             Console.WriteLine("_______________________________________");
             // Number 4
+            Console.WriteLine("Number 4");
             L2.Select(x => x * 2).ToList().ForEach(Console.WriteLine);
 
             Console.WriteLine("_______________________________________");
             // Number 5
+            Console.WriteLine("Number 5");
             var number5 = L1.Zip(L2, (x, y) => (long)x + y).Aggregate((x, y) => (x * y));
 
             Console.WriteLine(number5);
-
         }
-
-
-
     }
 }
